87091b0f2d45fc4cdd0d9c023d7c2f884103b3ec
Pavel Pscheidl <pavel@h2o.ai>
2019-01-07 14:19:52 +0100

    PUBDEV-6162 - S3 import with credentials

--- h2o-persist-s3/src/test/java/water/persist/PersistS3Test.java
+++ h2o-persist-s3/src/test/java/water/persist/PersistS3Test.java
@@ -8,18 +8,18 @@ import water.fvec.FileVec;
 import water.fvec.Frame;
 import water.util.FileUtils;
 
-import java.net.URI;
+import java.net.URI;;
 
 import static junit.framework.TestCase.assertEquals;
 
-/**
- * Created by tomas on 6/27/16.
- */
+
 public class PersistS3Test extends TestUtil {
 
 
   @BeforeClass
-  public static void setup() { stall_till_cloudsize(5); }
+  public static void setup() {
+    stall_till_cloudsize(1);
+  }
 
   private static class XORTask extends MRTask<XORTask> {
     long _res = 0;
@@ -57,4 +57,39 @@ public class PersistS3Test extends TestUtil {
       Scope.exit();
     }
   }
+
+  @Test
+  public void testS3ImportAccessProtected() throws Exception {
+    Scope.enter();
+    Key k = null, k2 = null;
+    Frame fr = null;
+    FileVec v = null, v2 = null;
+    try {
+      k = H2O.getPM().anyURIToKey(new URI("s3://test.0xdata.com/h2o-unit-tests/iris.csv"));
+      fr = DKV.getGet(k);
+      v = (FileVec) fr.anyVec();
+      // make sure we have some chunks
+      int chunkSize = (int) (v.length() / 3);
+      v.setChunkSize(fr, chunkSize);
+      long xor = new XORTask().doAll(v)._res;
+      k2 = H2O.getPM().anyURIToKey(new URI(FileUtils.getFile("smalldata/iris/iris.csv").getAbsolutePath()));
+      v2 = DKV.getGet(k2);
+      assertEquals(v2.length(), v.length());
+      assertVecEquals(v, v2, 0);
+      // make sure we have some chunks
+      v2.setChunkSize(chunkSize);
+      long xor2 = new XORTask().doAll(v2)._res;
+      assertEquals(xor2, xor);
+      fr.delete();
+      v2.remove();
+    } finally {
+      Scope.exit();
+      if (k != null) k.remove();
+      if (k2 != null) k2.remove();
+      if (fr != null) fr.remove();
+      if (v != null) v.remove();
+      if (v2 != null) v2.remove();
+    }
+  }
 }
+ 
