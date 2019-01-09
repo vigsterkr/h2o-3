7c5029bca41d0143d5c41ab80e5a30a6b6922403
Pavel Pscheidl <pavel@h2o.ai>
2019-01-09 12:57:39 +0100

    Tested in-URL credentials for PersistHDFS

--- h2o-persist-hdfs/src/test/java/water/persist/PersistS3HdfsTest.java
+++ h2o-persist-hdfs/src/test/java/water/persist/PersistS3HdfsTest.java
@@ -10,17 +10,25 @@ import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import water.H2O;
-import water.TestUtil;
+import water.*;
+import water.fvec.Chunk;
+import water.fvec.FileVec;
+import water.fvec.Frame;
+import water.util.FileUtils;
 import water.util.ReflectionUtils;
 
 import java.net.URI;
+import java.util.ArrayList;
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.*;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assume.assumeTrue;
 
 
 public class PersistS3HdfsTest extends TestUtil  {
+
+  private static final String AWS_ACCESS_KEY_PROPERTY_NAME = "AWS_ACCESS_KEY_ID";
+  private static final String AWS_SECRET_KEY_PROPERTY_NAME = "AWS_SECRET_ACCESS_KEY";
 
   @BeforeClass
   public static void setup() {
@@ -50,6 +58,77 @@ public class PersistS3HdfsTest extends TestUtil  {
     assertFalse(hdfsPersist.exists(existing)); // Our persist gives the same result
   }
 
+  @Test
+  public void testS3AImportAccessProtected() throws Exception {
+    // This test is only runnable in environment with Amazon credentials properly set {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY}
+    final String accessKey = System.getenv(AWS_ACCESS_KEY_PROPERTY_NAME);
+    final String secretKey = System.getenv(AWS_SECRET_KEY_PROPERTY_NAME);
+
+    assumeTrue(accessKey != null);
+    assumeTrue(secretKey != null);
+
+    StringBuilder s3UrlBuilder = new StringBuilder("s3a://");
+    s3UrlBuilder.append(accessKey);
+    s3UrlBuilder.append(":");
+    s3UrlBuilder.append(secretKey);
+    s3UrlBuilder.append("@h2o-unit-tests/iris.csv");
+
+    Scope.enter();
+    Key k = null, k2 = null;
+    Frame fr = null;
+    FileVec v = null, v2 = null;
+    try {
+      k = H2O.getPM().anyURIToKey(new URI(s3UrlBuilder.toString()));
+      fr = DKV.getGet(k);
+      assertNotNull(fr);
+      v = (FileVec) fr.anyVec();
+      // make sure we have some chunks
+      int chunkSize = (int) (v.length() / 3);
+      v.setChunkSize(fr, chunkSize);
+      long xor = new XORTask().doAll(v)._res;
+      k2 = H2O.getPM().anyURIToKey(new URI(FileUtils.getFile("smalldata/iris/iris.csv").getAbsolutePath()));
+      v2 = DKV.getGet(k2);
+      assertEquals(v2.length(), v.length());
+      assertVecEquals(v, v2, 0);
+      // make sure we have some chunks
+      v2.setChunkSize(chunkSize);
+      long xor2 = new XORTask().doAll(v2)._res;
+      assertEquals(xor2, xor);
+      fr.delete();
+      v2.remove();
+    } finally {
+      Scope.exit();
+      if (k != null) k.remove();
+      if (k2 != null) k2.remove();
+      if (fr != null) fr.remove();
+      if (v != null) v.remove();
+      if (v2 != null) v2.remove();
+    }
+  }
+
+  @Test
+  public void testS3CalcTypeaheadMatches() throws Exception {
+    // This test is only runnable in environment with Amazon credentials properly set {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY}
+    final String accessKey = System.getenv(AWS_ACCESS_KEY_PROPERTY_NAME);
+    final String secretKey = System.getenv(AWS_SECRET_KEY_PROPERTY_NAME);
+
+    assumeTrue(accessKey != null);
+    assumeTrue(secretKey != null);
+
+    StringBuilder s3UrlBuilder = new StringBuilder("s3a://");
+    s3UrlBuilder.append(accessKey);
+    s3UrlBuilder.append(":");
+    s3UrlBuilder.append(secretKey);
+    s3UrlBuilder.append("@h2o-unit-tests/iris.csv");
+
+    final PersistHdfs persistHdfs = new PersistHdfs();
+    final ArrayList<String> strings = persistHdfs.calcTypeaheadMatches(s3UrlBuilder.toString(), 10);
+    assertNotNull(strings);
+    assertEquals(1, strings.size());
+    // Test the resulting URL in a form of a string contains the access key and secret key.
+    assertEquals(s3UrlBuilder.toString(), strings.get(0));
+  }
+
   private Object getValue(Object o, String... fieldNames) {
     StringBuilder path = new StringBuilder(o.getClass().getName());
     for (String f : fieldNames) {
@@ -61,5 +140,20 @@ public class PersistS3HdfsTest extends TestUtil  {
     }
     return o;
   }
+
+private static class XORTask extends MRTask<XORTask> {
+  long _res = 0;
+
+  @Override
+  public void map(Chunk c) {
+    for (int i = 0; i < c._len; ++i)
+      _res ^= c.at8(i);
+  }
+
+  @Override
+  public void reduce(XORTask xort) {
+    _res = _res ^ xort._res;
+  }
+}
 
 }
