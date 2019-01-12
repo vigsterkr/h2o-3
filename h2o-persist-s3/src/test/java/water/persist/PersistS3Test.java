5498f5b5ddc834ece6d2b9251e887b670a8aa002
Pavel Pscheidl <pavel@h2o.ai>
2019-01-12 08:27:00 +0100

    Removed S3 test (do not confuse with PersistHdfs tests) - solved in another JIRA

--- h2o-persist-s3/src/test/java/water/persist/PersistS3Test.java
+++ h2o-persist-s3/src/test/java/water/persist/PersistS3Test.java
@@ -69,52 +69,5 @@ public class PersistS3Test extends TestUtil {
       if (v2 != null) v2.remove();
     }
   }
-
-  @Test
-  public void testS3ImportAccessProtected() throws Exception {
-    // This test is only runnable in environment with Amazon credentials properly set {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY}
-    final String accessKey = System.getenv(AWS_ACCESS_KEY_PROPERTY_NAME);
-    final String secretKey = System.getenv(AWS_SECRET_KEY_PROPERTY_NAME);
-    
-    assumeTrue(accessKey != null);
-    assumeTrue(secretKey != null);
-    
-    StringBuilder s3UrlBuilder = new StringBuilder("s3://");
-    s3UrlBuilder.append(accessKey);
-    s3UrlBuilder.append(":");
-    s3UrlBuilder.append(secretKey);
-    s3UrlBuilder.append("@test.0xdata.com/h2o-unit-tests/iris.csv");
-    
-    Scope.enter();
-    Key k = null, k2 = null;
-    Frame fr = null;
-    FileVec v = null, v2 = null;
-    try {
-      k = H2O.getPM().anyURIToKey(new URI(s3UrlBuilder.toString()));
-      fr = DKV.getGet(k);
-      v = (FileVec) fr.anyVec();
-      // make sure we have some chunks
-      int chunkSize = (int) (v.length() / 3);
-      v.setChunkSize(fr, chunkSize);
-      long xor = new XORTask().doAll(v)._res;
-      k2 = H2O.getPM().anyURIToKey(new URI(FileUtils.getFile("smalldata/iris/iris.csv").getAbsolutePath()));
-      v2 = DKV.getGet(k2);
-      assertEquals(v2.length(), v.length());
-      assertVecEquals(v, v2, 0);
-      // make sure we have some chunks
-      v2.setChunkSize(chunkSize);
-      long xor2 = new XORTask().doAll(v2)._res;
-      assertEquals(xor2, xor);
-      fr.delete();
-      v2.remove();
-    } finally {
-      Scope.exit();
-      if (k != null) k.remove();
-      if (k2 != null) k2.remove();
-      if (fr != null) fr.remove();
-      if (v != null) v.remove();
-      if (v2 != null) v2.remove();
-    }
-  }
 }
  
